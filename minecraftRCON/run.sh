b18a8767e99b13d942d169c91fb1e5fbcd53bad9
jcasallecchio <107600170+jcasallecchio@users.noreply.github.com>
2025-06-23 19:45:06 -0300

    Update run.sh

--- minecraftRCON/run.sh
+++ minecraftRCON/run.sh
@@ -5,9 +5,6 @@ set -e
 mkdir -p /share/minecraftRCON
 cd /share/minecraftRCON
 
-# Garante permissão de execução no binário (caso necessário)
-chmod +x /bedrock/bedrock_server
-
 # Inicia o servidor Bedrock como usuário não-root, com stdin monitorado para envio de 'stop'
 /usr/local/bin/entrypoint-demoter --match /share/minecraftRCON --debug --stdin-on-term stop /opt/bedrock-entry.sh &
 
