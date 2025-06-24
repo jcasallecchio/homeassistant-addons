a9122a59cf54844a82076617bc4d3f5ebf43ba64
jcasallecchio <107600170+jcasallecchio@users.noreply.github.com>
2025-06-24 01:51:01 -0300

    Update run.sh

--- MinecraftRCON/run.sh
+++ MinecraftRCON/run.sh
@@ -8,7 +8,7 @@ YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m' # Sem cor
 
-echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[ ############### Iniciando Minecraft Bedrock RCON ############### ]${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[ ############### Iniciando Minecraft Bedrock RCON ############### ]${NC}"
 
 SERVER_DIR="/share/minecraftRCON"
 SERVER_BIN="$SERVER_DIR/bedrock_server"
@@ -19,7 +19,7 @@ mkdir -p "$SERVER_DIR"
 # Se existir o zip, extrai e apaga para atualizar o servidor
 if [ -f "$SERVER_ZIP" ]; then
   total=$(unzip -l "$SERVER_ZIP" | grep -E '^[ ]+[0-9]' | wc -l)
-  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extraindo $total arquivos..."
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extraindo $total arquivos..."
   
   count=0
   last_percent=0
@@ -33,27 +33,27 @@ if [ -f "$SERVER_ZIP" ]; then
       fi
     fi
   done
-  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extração concluída! Removendo $SERVER_ZIP"
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extração concluída! Removendo $SERVER_ZIP"
   chmod +x "$SERVER_BIN"
   rm "$SERVER_ZIP"
 fi
 
 if [ ! -f "$SERVER_BIN" ]; then
-  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}Arquivo não encontrado. Coloque server.zip para extrair.${NC}"
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}Arquivo não encontrado. Coloque server.zip para extrair.${NC}"
   exit 1
 fi
 
 cd "$SERVER_DIR"
-echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando servidor Minecraft...${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando servidor Minecraft...${NC}"
 
 screen -dmS mc bash -c "./bedrock_server | tee /proc/1/fd/1"
 
 sleep 10
 
-echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando RCON personalizado...${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando RCON personalizado...${NC}"
 python3 /rcon_server.py &
 
-echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}Minecraft Bedrock RCON Server Online...${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}Minecraft Bedrock RCON Server Online...${NC}"
 
 wait
 exit $?
