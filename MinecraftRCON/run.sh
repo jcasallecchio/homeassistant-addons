d447a2cf790f6f55f1a9e5ca143fe93b45d4270f
jcasallecchio <107600170+jcasallecchio@users.noreply.github.com>
2025-06-24 01:42:21 -0300

    Update run.sh

--- MinecraftRCON/run.sh
+++ MinecraftRCON/run.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 set -e
 
@@ -15,26 +16,24 @@ SERVER_ZIP="$SERVER_DIR/server.zip"
 
 mkdir -p "$SERVER_DIR"
 
+# Se existir o zip, extrai e apaga para atualizar o servidor
 if [ -f "$SERVER_ZIP" ]; then
-  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Arquivo encontrado. Extraindo para atualização...${NC}"
+  total=$(unzip -l "$SERVER_ZIP" | grep -E '^[ ]+[0-9]' | wc -l)
+  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extraindo $total arquivos..."
   
-  total=$(unzip -l "$SERVER_ZIP" | tail -n +4 | head -n -2 | wc -l)
   count=0
   last_percent=0
-  TIMESTAMP="[$(date '+%Y-%m-%d %H:%M:%S')]"
-  
   unzip -o "$SERVER_ZIP" -d "$SERVER_DIR" | while read -r line; do
     if echo "$line" | grep -q "inflating:"; then
       count=$((count + 1))
       percent=$((count * 100 / total))
       if [ "$percent" -ne "$last_percent" ]; then
-        echo -ne "\r$TIMESTAMP Extraindo arquivos... $percent%%     "
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] Extraindo arquivos... $percent%"
         last_percent=$percent
       fi
     fi
   done
-  echo -e "\n[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extração concluída! Removendo $SERVER_ZIP${NC}"
-  
+  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extração concluída! Removendo $SERVER_ZIP"
   chmod +x "$SERVER_BIN"
   rm "$SERVER_ZIP"
 fi
@@ -54,7 +53,7 @@ sleep 10
 echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando RCON personalizado...${NC}"
 python3 /rcon_server.py &
 
-echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}✅ Minecraft Bedrock RCON Server Online...${NC}"
+echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}Minecraft Bedrock RCON Server Online...${NC}"
 
 wait -n
 exit $?
