fafbddd105c2e2e7f3366a06a7cd94f7a16c4641
jcasallecchio <107600170+jcasallecchio@users.noreply.github.com>
2025-06-24 01:31:34 -0300

    Update run.sh

--- MinecraftRCON/run.sh
+++ MinecraftRCON/run.sh
@@ -7,7 +7,7 @@ YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m' # Sem cor
 
-echo -e "${GREEN}[$(date '+%Y-%m-%d %H:%M:%S')] [ ############### Iniciando Minecraft Bedrock RCON ############### ]${NC}"
+echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[ ############### Iniciando Minecraft Bedrock RCON ############### ]${NC}"
 
 SERVER_DIR="/share/minecraftRCON"
 SERVER_BIN="$SERVER_DIR/bedrock_server"
@@ -16,46 +16,45 @@ SERVER_ZIP="$SERVER_DIR/server.zip"
 mkdir -p "$SERVER_DIR"
 
 if [ -f "$SERVER_ZIP" ]; then
-  echo -e "${YELLOW}[$(date '+%Y-%m-%d %H:%M:%S')] Arquivo encontrado. Extraindo para atualização...${NC}"
+  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Arquivo encontrado. Extraindo para atualização...${NC}"
   
-  # Conta total de arquivos no ZIP para o cálculo do %
   total=$(unzip -l "$SERVER_ZIP" | tail -n +4 | head -n -2 | wc -l)
   count=0
   last_percent=0
+  TIMESTAMP="[$(date '+%Y-%m-%d %H:%M:%S')]"
   
-  # Faz a extração e lê a saída linha a linha
   unzip -o "$SERVER_ZIP" -d "$SERVER_DIR" | while read -r line; do
     if echo "$line" | grep -q "inflating:"; then
       count=$((count + 1))
       percent=$((count * 100 / total))
       if [ "$percent" -ne "$last_percent" ]; then
-        echo -ne "\r[$(date '+%Y-%m-%d %H:%M:%S')] Extraindo arquivos... $percent%%"
+        echo -ne "\r$TIMESTAMP Extraindo arquivos... $percent%%     "
         last_percent=$percent
       fi
     fi
   done
-  echo -e "\n${YELLOW}[$(date '+%Y-%m-%d %H:%M:%S')] Extração concluída! Removendo $SERVER_ZIP${NC}"
+  echo -e "\n[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extração concluída! Removendo $SERVER_ZIP${NC}"
   
   chmod +x "$SERVER_BIN"
   rm "$SERVER_ZIP"
 fi
 
 if [ ! -f "$SERVER_BIN" ]; then
-  echo -e "${RED}[$(date '+%Y-%m-%d %H:%M:%S')] Arquivo não encontrado. Coloque server.zip para extrair.${NC}"
+  echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}Arquivo não encontrado. Coloque server.zip para extrair.${NC}"
   exit 1
 fi
 
 cd "$SERVER_DIR"
-echo -e "${GREEN}[$(date '+%Y-%m-%d %H:%M:%S')] Iniciando servidor Minecraft...${NC}"
+echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando servidor Minecraft...${NC}"
 
 screen -dmS mc bash -c "./bedrock_server | tee /proc/1/fd/1"
 
 sleep 10
 
-echo -e "${GREEN}[$(date '+%Y-%m-%d %H:%M:%S')] Iniciando RCON personalizado...${NC}"
+echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando RCON personalizado...${NC}"
 python3 /rcon_server.py &
 
-echo -e "${GREEN}[$(date '+%Y-%m-%d %H:%M:%S')] ✅ Minecraft Bedrock RCON Server Online...${NC}"
+echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}✅ Minecraft Bedrock RCON Server Online...${NC}"
 
 wait -n
 exit $?
