7b167dd275226ebee6bc9b1b1c314ab991c13b8f
jcasallecchio <107600170+jcasallecchio@users.noreply.github.com>
2025-06-24 01:57:01 -0300

    Update run.sh

--- MinecraftRCON/run.sh
+++ MinecraftRCON/run.sh
@@ -6,9 +6,8 @@ set -e
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
-NC='\033[0m' # Sem cor
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[ ############### Iniciando Minecraft Bedrock RCON ############### ]${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[ ############### Iniciando Minecraft Bedrock RCON ############### ]"
 
 SERVER_DIR="/share/minecraftRCON"
 SERVER_BIN="$SERVER_DIR/bedrock_server"
@@ -19,7 +18,8 @@ mkdir -p "$SERVER_DIR"
 # Se existir o zip, extrai e apaga para atualizar o servidor
 if [ -f "$SERVER_ZIP" ]; then
   total=$(unzip -l "$SERVER_ZIP" | grep -E '^[ ]+[0-9]' | wc -l)
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extraindo $total arquivos..."
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Arquivo server.zip encontrado, iniciando extração..."
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Extraindo arquivos... Total: $total"
   
   count=0
   last_percent=0
@@ -39,21 +39,21 @@ if [ -f "$SERVER_ZIP" ]; then
 fi
 
 if [ ! -f "$SERVER_BIN" ]; then
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}Arquivo não encontrado. Coloque server.zip para extrair.${NC}"
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}Arquivo não encontrado. Coloque server.zip para extrair."
   exit 1
 fi
 
 cd "$SERVER_DIR"
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando servidor Minecraft...${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando servidor Minecraft..."
 
 screen -dmS mc bash -c "./bedrock_server | tee /proc/1/fd/1"
 
 sleep 10
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando RCON personalizado...${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}Iniciando RCON personalizado..."
 python3 /rcon_server.py &
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}Minecraft Bedrock RCON Server Online...${NC}"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}Minecraft Bedrock RCON Server Online..."
 
 wait
 exit $?
