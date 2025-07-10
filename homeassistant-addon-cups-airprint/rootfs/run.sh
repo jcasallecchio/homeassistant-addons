03eb4bfb3e030d6d5aead83b53a35eb2228e0474
jcasallecchio <107600170+jcasallecchio@users.noreply.github.com>
2025-07-10 19:06:02 -0300

    Add files via upload

---
+++ homeassistant-addon-cups-airprint/rootfs/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+
+ulimit -n 1048576
+
+until [ -e /var/run/avahi-daemon/socket ]; do
+  sleep 1s
+done
+
+bashio::log.info "Preparing directories"
+if [ ! -d /config/cups ]; then cp -v -R /etc/cups /config; fi
+rm -v -fR /etc/cups
+
+ln -v -s /config/cups /etc/cups
+
+bashio::log.info "Starting CUPS server as CMD from S6"
+
+cupsd -f
